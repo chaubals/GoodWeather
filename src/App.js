216d37c7faa4880f0aed04c784665b5b7bf5b7c9
Siddhesh Chaubal <siddhesh.chaubal2024@gmail.com>
2024-10-01 23:14:57 -0400

    Heading CSS made more mobile-friendly

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
-      <h1 className="text-4xl sm:text-3xl font-bold mb-4 text-blue-800 -m-10">Welcome to GoodWeather!!</h1>
+      <h1 className="text-4xl font-bold mb-4 text-blue-600 sm:text-3xl md:text-2xl mx-4">Welcome to GoodWeather!!</h1>
       <input
         type="text"
         value={city}
